5f724b614cf1e98fecfa0bc2aded41cfc931bf35
unknown <dayvidallan@hotmail.com>
2021-02-02 15:35:37 -0300

    primeira template

--- website/views.py
+++ website/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
 
-# Create your views here.
+
+def blog(request):
+    lista = [
+        'Django', 'Python', 'git', 'Html'
+    ]
+    data = {'name': 'site_carol', 'lista_tecnologias': lista}
+    return render(request, 'index.html', data)
