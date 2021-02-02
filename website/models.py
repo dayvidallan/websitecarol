31bdeda75128b428fabcbba306dee613514427f9
unknown <dayvidallan@hotmail.com>
2021-02-02 16:30:52 -0300

    atualizando as templates

--- website/models.py
+++ website/models.py
@@ -6,5 +6,8 @@ class Post(models.Model):
     sub_title = models.CharField(max_length=200)
     content = models.TextField()
 
+    def __str__(self):
+        return self.title
+
 
 
--- website/views.py
+++ website/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
 
 def blog(request):
     lista = [
         'Django', 'Python', 'git', 'Html'
     ]
-    data = {'name': 'site_carol', 'lista_tecnologias': lista}
+
+    list_posts = Post.objects.all()
+    data = {'name': 'site_carol',
+            'lista_tecnologias': lista,
+            'posts': list_posts
+            }
+
     return render(request, 'index.html', data)
