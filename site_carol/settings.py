0df95e5b2ac97e03a80fca1c455d916e588562a6
unknown <dayvidallan@hotmail.com>
2021-02-03 19:05:11 -0300

    corrigindo o deploy

--- site_carol/settings.py
+++ site_carol/settings.py
@@ -92,8 +92,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-MIDIA_URL = '/midia/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
