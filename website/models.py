4f5ca3be5e60623bcaa21c90613264fd4a7ac743
unknown <dayvidallan@hotmail.com>
2021-02-09 16:32:09 -0300

    corrigindo erros das templates

--- website/models.py
+++ website/models.py
@@ -15,7 +15,7 @@ class Contact(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=100)
     sub_title = models.CharField(max_length=200)
-    content = RichTextField(null=True, blank=True)
+    content = models.TextField(null=True, blank=True)
     deleted = models.BooleanField(default=False)
     imagem = models.ImageField(upload_to='posts', null=True, blank=True)
 
@@ -26,8 +26,13 @@ class Post(models.Model):
 class Perfil(models.Model):
     name = models.CharField(max_length=200, null=True, blank=True)
     especialidade = models.CharField(max_length=200, null=True, blank=True)
-    texto = RichTextField(null=True, blank=True)
+    endereco = models.CharField(max_length=200, null=True, blank=True)
+    bairro = models.CharField(max_length=200, null=True, blank=True)
+    cidade = models.CharField(max_length=200, null=True, blank=True)
+    estado = models.CharField(max_length=200, null=True, blank=True)
+    texto = models.TextField(null=True, blank=True)
     apresent = models.TextField(null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
     nrTelCelular = models.CharField(max_length=11, blank=True, null=True, verbose_name='Nº telefone celular')
     imagem = models.ImageField(upload_to='perfils', null=True, blank=True)
 
--- website/views.py
+++ website/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+
+
 from .models import Post, Perfil, Contact
 
 
@@ -22,13 +24,10 @@ def post_detail(request, id):
 
     post = Post.objects.get(id=id)
 
-    meu_perfil = Perfil.objects.all()
+    return render(request, 'post_detail.html', {'post': post})
 
-    data = {
-        'post': post,
-        'perfil': meu_perfil,
-    }
-    return render(request, 'post_detail.html', data)
+
+
 
 
 def save_form(request):
